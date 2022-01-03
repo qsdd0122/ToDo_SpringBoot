eede4e3e8a8d4fb240a10e275a249afaad503169
actic1014 <59953581+actic1014@users.noreply.github.com>
2022-01-03 14:37:01 +0900

    Service실습

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,14 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
+import com.example.demo.service.TodoService;
+
+import lombok.Builder;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
 	
+	// controller 실습
 	@GetMapping
 	public String testController() {
-		return "Hello World!s";
+		return "Hello World!";
 	}
 	
 	@GetMapping("/testGetMapping")
@@ -32,6 +38,7 @@ public class TestController {
 		return "Hello World! ID " + id;
 	}
 	
+	//RequestParam을 이용한 매개변수 전달
 	@GetMapping("/testRequestParam")
 	public String testControllerRequestParam(@RequestParam(required = false) int id) {
 		return "Hello World! ID" + id;
@@ -42,6 +49,7 @@ public class TestController {
 		return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
 	}
 	
+	//ResponseDTO를 반환하는 컨트롤러 메소드
 	@GetMapping("/testResponseBody")
 	public ResponseDTO<String> tesControllerResponseBody(){
 		List<String> list = new ArrayList<>();
@@ -50,4 +58,15 @@ public class TestController {
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return response;
 	}
+	
+	//ResponseEntity를 반환하는 컨트롤러 메소드
+	@GetMapping("/testResponseEntity")
+	public ResponseEntity<?> testControllerResponseEntity(){
+		List<String> list = new ArrayList<>();
+		list.add("Hello World! I'm ResponseEntity. And you got 400!");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		//http status를 400으로 설정
+		return ResponseEntity.badRequest().body(response);
+	}
+
 }
---
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.service.TodoService;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+	/*  Service 실습   */
+	
+	//Qutowired : 알아서 빈을 찾은 후 그 빈을 인스턴스 멤버 변수에 연결
+	@Autowired
+	private TodoService service;
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> testTodo(){
+		String str = service.testService();
+		List<String> list = new ArrayList<>();
+		list.add(str);
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
+	}
+}
