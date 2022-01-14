56b3c4eb94beade08ab6cbf5bd8eceec00ec7b5e
actic1014 <96825988+qsdd0122@users.noreply.github.com>
2022-01-14 14:17:44 +0900

    JWT토큰 생성/반환

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.UserDTO;
 import com.example.demo.model.UserEntity;
+import com.example.demo.security.TokenProvider;
 import com.example.demo.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,8 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private TokenProvider tokenProvider;
 	
 	@PostMapping("/signup")
 	public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
@@ -51,9 +54,12 @@ public class UserController {
 		UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
 		
 		if(user != null) {
+			//토큰생성
+			final String token = tokenProvider.create(user);
 			final UserDTO responseUserDTO = UserDTO.builder()
 				.email(user.getUsername())
 				.id(user.getId())
+				.token(token)
 				.build();
 			return ResponseEntity.ok().body(responseUserDTO);
 		} else {
---
+++ src/main/java/com/example/demo/security/TokenProvider.java
@@ -0,0 +1,52 @@
+package com.example.demo.security;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.UserEntity;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+//사용자 정보를 받아 JWT를 생성하는 클래스
+public class TokenProvider {
+	private static final String SECRET_KEY = "NMA8JPctFuna59f5";
+	
+	// JWT 토큰을 생성하는 메소드
+	public String create(UserEntity userEntity) {
+		// 기한은 지금부터 1일로 설정
+		Date expiryDate = Date.from(
+			Instant.now().plus(1, ChronoUnit.DAYS));
+
+		//JWT Token 생성
+		return Jwts.builder()
+			//header에 들어갈 내용 및 서명을 하기 위한 SECRET_KEY
+			.signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+			//payload에 들어갈 내용
+			.setSubject(userEntity.getId())
+			.setIssuer("demo app")
+			.setIssuedAt(new Date())
+			.setExpiration(expiryDate)
+			.compact();
+	}
+	
+	// 토큰을 디코딩 및 파싱하고 토큰의 위조 여부 확인
+	public String validateAndGetUserId(String token) {
+		//parseClaimsJws 메서드가 Base64호 디코딩 및 파싱
+		//헤더와 페이로드를 setSigningKey로 넘어온 시크릿을 이용해 서명한 후 token의 서명과 비교
+		//위조되지 않았다면 페이로드(Claims)를 리턴, 위조라면 예외를 날림
+		//그 중 userId가 필요하므로 getBody를 부른다.
+		Claims claims = Jwts.parser().setSigningKey(SECRET_KEY)
+				.parseClaimsJws(token).getBody();
+		
+		return claims.getSubject();
+	}
+	
+}
